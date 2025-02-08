0318523fbedd00a9a02b33df77f36e33fcc36db5
U220717056 <meliskirik@posta.mu.edu.tr>
2025-02-08 17:45:07 +0300

    URL Configuration

--- safeCodeProvider/student/urls.py
+++ safeCodeProvider/student/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.student_login, name='student_login'),  # Giriş sayfası
-    path('exam/', views.exam_page, name='exam_page'),     # Sınav sayfası (ileride ekleyeceğiz)
+    path('exam/', views.exam_page, name='exam_page'),     # Sınav sayfası
+    path('control/', views.student_control, name='student_control'),  # API: Öğrenci giriş kontrolü
 ]
