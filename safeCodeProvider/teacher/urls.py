29e3e7dcb232573a0bb1062e29e55c426817f3a3
mazlumemre <mazlumemregirgin@msn.com>
2025-02-12 21:58:58 +0300

    submits url added

--- safeCodeProvider/teacher/urls.py
+++ safeCodeProvider/teacher/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import teacher_home, upload_files, start_exam
+from .views import teacher_home, upload_files, start_exam, submits_page
 
 urlpatterns = [
     path('', teacher_home, name='teacher_home'),
     path('upload/', upload_files, name='upload_files'),
     path('start_exam/', start_exam, name='start_exam'),
+     path('submits/', submits_page, name='submits_page'),
 ]
