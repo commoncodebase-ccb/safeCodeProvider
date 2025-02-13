d147739889da6af8514f67d38505ec9ff793c45c
Serdar1048 <serdardedebas1@gmail.com>
2025-02-13 17:45:59 +0300

    exam instruaction is added to exam_page

--- safeCodeProvider/student/urls.py
+++ safeCodeProvider/student/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path  
 from . import views 
 
@@ -5,4 +7,4 @@ urlpatterns = [
     path('', views.student_login, name='student_login'),  
     path('exam/', views.exam_page, name='exam_page'),  
     path('control/', views.student_control, name='student_control'),  # Öğrenci giriş kontrolünü sağlayan API URL'si
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
