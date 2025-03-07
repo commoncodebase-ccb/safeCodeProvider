5d6e0bd4ad2e2b467fcfd47829642d813e1313b1
mazlumemre <mazlumemregirgin@msn.com>
2025-03-07 16:03:11 +0300

    deleted folders

--- safeCodeProvider/teacher/management/commands/runserver.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.commands.runserver import Command as RunserverCommand
-
-class Command(RunserverCommand):
-    def add_arguments(self, parser):
-        super().add_arguments(parser)
-        parser.set_defaults(port="8000")  # Buraya app1 için kullanacağın portu yaz
