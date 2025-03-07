ba7543f94f1e0fdb046db916af6a8d93baf7d8b7
mazlumemre <mazlumemregirgin@msn.com>
2025-03-07 16:02:45 +0300

    settings config added for port section

---
+++ safeCodeProvider/safeCodeProvider/settings/student_settings.py
@@ -0,0 +1,6 @@
+from .settings import *
+
+INSTALLED_APPS += ['student']
+
+# app2 sadece 8002 portunda çalışsın
+PORT = 8001
---
+++ safeCodeProvider/safeCodeProvider/settings/teacher_settings.py
@@ -0,0 +1,6 @@
+from .settings import *
+
+INSTALLED_APPS += ['teacher']
+
+# app1 sadece 8000 portunda çalışsın
+PORT = 8000
