0ae38040aff5cb9a657825a63e9e3cab2dd73f36
yaren <136489994+yarennoztekinn@users.noreply.github.com>
2025-03-11 18:33:57 +0300

    student's codes are brought to the submits page

--- safeCodeProvider/teacher/urls.py
+++ safeCodeProvider/teacher/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import teacher_home, upload_files, start_exam, submits_page
+from .views import teacher_home, upload_files, start_exam, submits_page, bring_code
 
 urlpatterns = [
     path('', teacher_home, name='teacher_home'),
     path('upload/', upload_files, name='upload_files'),
     path('start_exam/', start_exam, name='start_exam'),
-     path('submits/', submits_page, name='submits_page'),
+    path('submits/', submits_page, name='submits_page'),
+    path('bring_code/', bring_code, name='bring_code'),
 ]
