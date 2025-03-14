590a90aa29cd3be0daec70bb64d9bfbed6afba87
mazlumemre <mazlumemregirgin@msn.com>
2025-03-14 16:32:28 +0300

    port urls added.

--- safeCodeProvider/teacher/urls.py
+++ safeCodeProvider/teacher/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import teacher_home, upload_files, start_exam, submits_page, bring_code
+from .views import teacher_home, upload_files, start_exam, submits_page, bring_code, open_student_port, close_student_port
 
 urlpatterns = [
     path('', teacher_home, name='teacher_home'),
@@ -7,4 +7,8 @@ urlpatterns = [
     path('start_exam/', start_exam, name='start_exam'),
     path('submits/', submits_page, name='submits_page'),
     path('bring_code/', bring_code, name='bring_code'),
+    path("open-port/", open_student_port, name="open_port"),
+    path("close-port/", close_student_port, name="close_port"),
+
+
 ]
