54998fe41207b85508ad8136611f5ee66cf22668
mazlum emre girgin <mazlumemregirgin@msn.com>
2025-05-01 19:53:05 +0300

    get_config and get_file_content func path added.

--- safeCodeProvider/student/urls.py
+++ safeCodeProvider/student/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path  
 from . import views 
+from .views import get_file_content
+
 
 urlpatterns = [
     path('', views.student_login, name='student_login'),  
@@ -10,6 +12,8 @@ urlpatterns = [
     path('save_code/', views.save_code, name='save_code'),
     path('run_code/', views.run_code, name='run_code'),
     path('delete_docker/', views.delete_docker, name='delete_docker'),
+    path("get_config/", views.get_config, name="get_config"),
+    path("get_file_content/", get_file_content, name="get_file_content"),
     # path("get_exam_time/", views.get_exam_time, name="get_exam_time"),
 
 
