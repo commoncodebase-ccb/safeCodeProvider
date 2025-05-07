0a0a9711f240edccd9644197e926d33f7a1399eb
yaren <136489994+yarennoztekinn@users.noreply.github.com>
2025-05-07 18:35:22 +0300

    working with multifile operations are on going

--- safeCodeProvider/teacher/views.py
+++ safeCodeProvider/teacher/views.py
@@ -27,6 +27,10 @@ ALLOWED_EXTENSIONS = {
 def teacher_home(request):
     return render(request, 'teacher_home.html')
 
+def convert_to_ascii(text):
+    turkish_map = str.maketrans("çğıöşüÇĞİÖŞÜ", "cgiosuCGIOSU")
+    return text.translate(turkish_map)
+
 def save_uploaded_file(uploaded_file, file_type):
     save_path = os.path.join(settings.MEDIA_ROOT, file_type)
     os.makedirs(save_path, exist_ok=True)
@@ -37,46 +41,6 @@ def save_uploaded_file(uploaded_file, file_type):
             destination.write(chunk)
 
     return uploaded_file.name
-
-def upload_files(request):
-    if request.method == 'POST':
-        uploaded_files = {}
-
-        # Tekli dosyalar
-        for file_type in ['student_list', 'exam_instruction']:
-            uploaded_file = request.FILES.get(file_type)
-            if not uploaded_file:
-                return JsonResponse({'error': f'{file_type} Missing file!'}, status=400)
-
-            allowed_extensions = ALLOWED_EXTENSIONS.get(file_type)
-            file_extension = uploaded_file.name.split('.')[-1].lower()
-            if file_extension not in allowed_extensions:
-                return JsonResponse({'error': f'{file_type} sadece {", ".join(allowed_extensions)} formatında olmalıdır!'}, status=400)
-
-            saved_name = save_uploaded_file(uploaded_file, file_type)
-            uploaded_files[file_type] = saved_name
-
-        # Çoklu assignment dosyaları
-        assignment_files = request.FILES.getlist('assignment_files')
-        if not assignment_files:
-            return JsonResponse({'error': 'assignment_files Missing file(s)!'}, status=400)
-
-        uploaded_files['assignment_files'] = []
-
-        for uploaded_file in assignment_files:
-            file_extension = uploaded_file.name.split('.')[-1].lower()
-            if file_extension not in ALLOWED_EXTENSIONS['assignment_files']:
-                return JsonResponse({'error': f'assignment_files sadece {", ".join(ALLOWED_EXTENSIONS["assignment_files"])} formatında olmalıdır!'}, status=400)
-
-            saved_name = save_uploaded_file(uploaded_file, 'assignment_files')
-            uploaded_files['assignment_files'].append(saved_name)
-
-        return JsonResponse({
-            'message': 'All files uploaded successfully',
-            'files': uploaded_files
-        })
-
-    return JsonResponse({'error': 'invalid request'}, status=400)
 
 def convert_csv_to_json(csv_filename):
     # Generate full path to CSV file
@@ -123,13 +87,35 @@ def start_exam(request):
 
             student_list_name = save_uploaded_file(student_list, "student_list")
             exam_instruction_name = save_uploaded_file(exam_instruction, "exam_instruction")
+            convert_csv_to_json(student_list_name)
 
             assignment_file_names = []
+
             for file in assignment_files:
                 saved_name = save_uploaded_file(file, "assignment_files")
                 assignment_file_names.append(saved_name)
 
-            convert_csv_to_json(student_list_name)
+            # En son yüklenen dosyadan türünü bul (örn: py, java)
+            file_type = "unknown"
+            file_name = "unknown"
+
+            if assignment_file_names:
+                last_file = assignment_file_names[-1]
+                file_name, file_extension = os.path.splitext(last_file)
+                file_type = file_extension.lstrip(".")
+
+            java_main_file_found = False
+
+            if file_type == 'java': #main dosyasi var mi kontrolu
+                for file in assignment_file_names:
+                    name = file.split('.')[0].lower()
+                    if name.lower() == 'main':
+                        java_main_file_found = True
+                        break
+
+                if not java_main_file_found:
+                    return JsonResponse({'error': 'To run java code, the file containing the public static void main function must be named main'}, status=400)
+                     
 
             config_path = "config.json"
             exam_data = {}
@@ -140,14 +126,6 @@ def start_exam(request):
                         exam_data = json.load(file)
                     except json.JSONDecodeError:
                         exam_data = {}
-
-            # En son yüklenen dosyadan türünü bul (örn: py, java)
-            file_type = "unknown"
-            file_name = "unknown"
-            if assignment_file_names:
-                last_file = assignment_file_names[-1]
-                file_name, file_extension = os.path.splitext(last_file)
-                file_type = file_extension.lstrip(".")
 
             exam_data.update({
                 "exam_name": exam_name,
@@ -170,153 +148,153 @@ def start_exam(request):
 
     return JsonResponse({"error": "Geçersiz istek yöntemi"}, status=405)
 
-def convert_to_ascii(text):
-    turkish_map = str.maketrans("çğıöşüÇĞİÖŞÜ", "cgiosuCGIOSU")
-    return text.translate(turkish_map)
+def create_student_dockerfile(exam_type, file_name, dockerfile_path, folder_path):
+    with open(dockerfile_path, "r") as file:
+        dockerfile_lines = file.readlines()
+
+    if exam_type == "py":
+        cmd_line = f'CMD ["/bin/sh", "-c", "python {file_name}.py"]\n'
+    elif exam_type == "java":
+        package_name = ""
+        with open(os.path.join(folder_path, "Main.java")) as f:
+            for line in f:
+                if line.strip().startswith("package "):
+                    package_name = line.strip().split()[1].rstrip(";")
+                    break
+        class_path = f"{package_name}.Main" if package_name else "Main"
+        cmd_line = (
+            'CMD ["sh", "-c", "find /app -name \'*.java\' > /app/sources.txt && '
+            'javac @/app/sources.txt && '
+            f'java -cp /app {class_path}"]\n'
+        )
+    elif exam_type == "c":
+        cmd_line = f'CMD ["/bin/sh", "-c", "gcc -o {file_name} {file_name}.c && ./{file_name}"]\n'
+    else:
+        raise ValueError(f"Unsupported file type: {exam_type}")
+
+    dockerfile_lines[-1] = cmd_line
+    student_dockerfile_path = os.path.join(folder_path, "Dockerfile")
+
+    with open(student_dockerfile_path, "w") as file:
+        file.writelines(dockerfile_lines)
+
+def prepare_student_folders(csv_file_path, script_dir, uploads_dir):
+    students = []
+    with open(csv_file_path, "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if len(row) >= 2:
+                student_id = row[0]
+                student_name = row[1].strip().lower().replace(" ", "-")
+                student_name = convert_to_ascii(student_name)
+                folder_name = f"{student_id}_{student_name}"
+                students.append(folder_name)
+    
+    os.makedirs(uploads_dir, exist_ok=True)
+
+    for student in students:
+        folder_path = os.path.join(uploads_dir, student)
+        os.makedirs(folder_path, exist_ok=True)
+
+        for script_file in os.listdir(script_dir):
+            script_file_path = os.path.join(script_dir, script_file)
+            shutil.copy(script_file_path, os.path.join(folder_path, script_file))
+
+    return students
+
+def create_docker_images(students, uploads_dir):
+    for student in students:
+        safe_student_name = student.replace("_", "-")
+        os.system(f"docker build -t {safe_student_name} {os.path.join(uploads_dir, student)}")
+
+def manage_containers(students):
+    for student in students:
+        safe_student_name = student.replace("_", "-")
+        container_name = f"{safe_student_name}-container"
+
+        result = subprocess.run(
+            ["docker", "ps", "-a", "-q", "--filter", f"name={container_name}"],
+            capture_output=True, encoding="utf-8", text=True
+        )
+
+        container_id = result.stdout.strip()
+        if container_id:
+            subprocess.run(["docker", "rm", "-f", container_id])
+
+        subprocess.run(["docker", "run", "-d", "--name", container_name, safe_student_name, "tail", "-f", "/dev/null"])
+
+def execute_commands_in_containers(students, exam_type, file_name):
+    for student in students:
+        safe_student_name = student.replace("_", "-")
+        if exam_type == "py":
+            docker_exec_cmd = f"docker exec {safe_student_name}-container python /app/{file_name}.py"
+        elif exam_type == "java":
+            docker_exec_cmd = f'docker exec {safe_student_name}-container sh -c "javac /app/*.java && java -cp /app {file_name}"'
+        elif exam_type == "c":
+            docker_exec_cmd = f"docker exec {safe_student_name}-container gcc /app/{file_name}.c -o /app/{file_name} && docker exec {safe_student_name}-container /app/{file_name}"
+        else:
+            raise ValueError(f"Unsupported file type: {exam_type}")
+
+        try:
+            result = subprocess.run(
+                docker_exec_cmd, shell=True, capture_output=True, text=True
+            )
+
+            if result.returncode == 0:
+                print(result.stdout)
+            else:
+                print(f"Error executing script in container for {safe_student_name}:")
+                print(result.stderr)
+
+        except Exception as e:
+            print(f"An error occurred while executing the script for {safe_student_name}: {str(e)}")
 
 def handle_docker_operations(config_path, request):
     try:
-        # Read config.json file
         with open(config_path, "r", encoding="utf-8") as file:
             config = json.load(file)
 
         exam_type = config.get("type", "py").lower()
-        file_name = config.get("file_name", "script").lower()
+        file_name = "Main" if exam_type == "java" else config.get("file_name", "script").lower()
 
-        # Choosing Dockerfile based on exam type
         dockerfile_map = {
             "py": "docker/python.Dockerfile",
             "java": "docker/java.Dockerfile",
             "c": "docker/c.Dockerfile"
         }
-
         dockerfile_path = os.path.join(os.getcwd(), dockerfile_map.get(exam_type))
 
-        if not dockerfile_path or not os.path.exists(dockerfile_path):
+        if not os.path.exists(dockerfile_path):
             return JsonResponse({"error": f"Unsupported or missing Dockerfile for exam type: {exam_type}"}, status=400)
 
-        csv_dir = "media/student_list"  # CSV file path
+        csv_dir = "media/student_list"
         csv_files = os.listdir(csv_dir)
-
         if not csv_files:
             return JsonResponse({"error": "No CSV file found in student_list folder"}, status=400)
 
         csv_file_path = os.path.join(csv_dir, csv_files[0])
-
-        script_dir = "media/assignment_files"  # Assignment files directory
+        script_dir = "media/assignment_files"
         script_files = os.listdir(script_dir)
-
         if not script_files:
             return JsonResponse({"error": "No assignment files found in assignment_file folder"}, status=400)
 
-        # Creating "uploads" directory to hold student-specific files
         uploads_dir = "uploads"
-        os.makedirs(uploads_dir, exist_ok=True)
+        students = prepare_student_folders(csv_file_path, script_dir, uploads_dir)
 
-        # Read CSV file and create a folder for each student
-        students = []
-        with open(csv_file_path, "r", encoding="utf-8") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                if len(row) >= 2:
-                    student_id = row[0]
-                    student_name = row[1].strip().lower().replace(" ", "-")
-                    student_name = convert_to_ascii(student_name)
-                    container_name = f"{student_id}-{student_name}-container"
-                    folder_name = f"{student_id}_{student_name}"
-                    students.append(folder_name)
-
-        # Create student folders and copy all assignment files
         for student in students:
-            folder_path = os.path.join(uploads_dir, student)  # uploads/studentID_studentName
-            os.makedirs(folder_path, exist_ok=True)
+            folder_path = os.path.join(uploads_dir, student)
+            create_student_dockerfile(exam_type, file_name, dockerfile_path, folder_path)
 
-            # Read the Dockerfile and modify it for the student
-            with open(dockerfile_path, "r") as file:
-                dockerfile_lines = file.readlines()
-
-            # Make CMD command student specific
-            if exam_type == "py":
-                cmd_line = f'CMD ["/bin/sh", "-c", "python {file_name}.py"]\n'
-            elif exam_type == "java":
-                cmd_line = f'CMD ["/bin/sh", "-c", "javac {file_name}.java && java -cp . {file_name}"]\n'
-            elif exam_type == "c":
-                cmd_line = f'CMD ["/bin/sh", "-c", "gcc -o {file_name} {file_name}.c && ./{file_name}"]\n'
-            else:
-                return JsonResponse({"error": f"Unsupported file type: {exam_type}"}, status=400)
-
-            # Replace last line with custom CMD
-            dockerfile_lines[-1] = cmd_line
-
-            student_dockerfile_path = os.path.join(folder_path, "Dockerfile")  # uploads/studentID_studentName/Dockerfile
-            with open(student_dockerfile_path, "w") as file:
-                file.writelines(dockerfile_lines)
-
-            # Copy all assignment files into the student folder
-            for script_file in script_files:
-                script_file_path = os.path.join(script_dir, script_file)
-                shutil.copy(script_file_path, os.path.join(folder_path, script_file))
-
-        # Create Docker images for each student
-        for student in students:
-            safe_student_name = student.replace("_", "-")
-            os.system(f"docker build -t {safe_student_name} {os.path.join(uploads_dir, student)}")
-
-        # Remove any pre-existing containers and run new containers
-        for student in students:
-            safe_student_name = student.replace("_", "-")
-            container_name = f"{safe_student_name}-container"
-
-            # Find the existing container
-            result = subprocess.run(
-                ["docker", "ps", "-a", "-q", "--filter", f"name={container_name}"],
-                capture_output=True, encoding="utf-8", text=True
-            )
-
-            container_id = result.stdout.strip()  # If there is an ID, get it, otherwise it will be an empty string
-
-            # If there is a container, remove it
-            if container_id:
-                subprocess.run(["docker", "rm", "-f", container_id])
-
-            # Start a new container
-            subprocess.run(["docker", "run", "-d", "--name", container_name, safe_student_name, "tail", "-f", "/dev/null"])
-
-        # Execute the command inside the container
-        for student in students:
-            safe_student_name = student.replace("_", "-")
-
-            if exam_type == "py":
-                docker_exec_cmd = f"docker exec {safe_student_name}-container python /app/{file_name}.py"
-            elif exam_type == "java":
-                docker_exec_cmd = f"docker exec {safe_student_name}-container javac /app/{file_name}.java && docker exec {safe_student_name}-container java -cp /app {file_name}"
-            elif exam_type == "c":
-                docker_exec_cmd = f"docker exec {safe_student_name}-container gcc /app/{file_name}.c -o /app/{file_name} && docker exec {safe_student_name}-container /app/{file_name}"
-            else:
-                return JsonResponse({"error": f"Unsupported file type: {exam_type}"}, status=400)
-
-            try:
-                result = subprocess.run(
-                    docker_exec_cmd,
-                    shell=True,
-                    capture_output=True,
-                    text=True
-                )
-
-                # Check if the command was successful
-                if result.returncode == 0:
-                    print(result.stdout)  # This contains the output of the Python script
-                else:
-                    print(f"Error executing script in container for {safe_student_name}:")
-                    print(result.stderr)  # This contains any error messages
-
-            except Exception as e:
-                print(f"An error occurred while executing the script for {safe_student_name}: {str(e)}")
+        create_docker_images(students, uploads_dir)
+        manage_containers(students)
+        execute_commands_in_containers(students, exam_type, file_name)
 
         return JsonResponse({"message": "Docker işlemleri başarıyla tamamlandı!"})
 
     except Exception as e:
         return JsonResponse({"error": f"Docker işlemleri sırasında hata oluştu: {str(e)}"}, status=500)
+
+
 
 
 def submits_page(request):
